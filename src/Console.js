462676e45b7af186a44e2bbb12f8f4333487d493
Bogdan Luca <luca.bogdan@gmail.com>
2017-10-12 16:41:44 +0300

    Changed style.

--- src/Console.js
+++ src/Console.js
@@ -11,11 +11,6 @@ const containerStyle = {
   fontSize: 'small',
   overflow: 'auto',
   padding: 2,
-  position: 'absolute',
-  top: 0,
-  bottom: 0,
-  left: 0,
-  right: 0
 }
 
 class Console extends Component {
